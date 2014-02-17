2792ed5b1c0975c57324e76c8068613955b013db
Omair Ali <omair@Peanut>
2014-02-17 17:17:09 +1100

    Pages now have titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+   # Generates a full title for the page
+   def get_title(page_title)
+      base_title = "Game Dev Soc UNSW"
+      if (page_title.empty?)
+         return base_title
+      else
+         return "#{base_title} | #{page_title}"
+      end
+   end
 end
