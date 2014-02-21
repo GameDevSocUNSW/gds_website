c10cdeaa3425c0a29ad40b8014250723a151fb8c
Omair Ali <omair@Peanut>
2014-02-22 04:22:22 +1100

    pgt

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
 
    def get_author_name(post)
-      user = User.find_by(post.user_id)
+      user = User.find_by(id: post.user_id)
       return "#{user.first_name} #{user.last_name} [#{user.position}]"
    end
 end
