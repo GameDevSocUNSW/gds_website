7f9b1ee5b3ef53d5da774603086de76c22f54c76
Omair Ali <omair@Peanut>
2014-02-21 15:41:17 +1100

    Sign in/Sign out working

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+   include SessionsHelper
+   def new
+   end
+   def create
+      @error=''
+      user = User.find_by(position: params[:session][:position].capitalize)
+      if (user && user.authenticate(params[:session][:password]))
+         sign_in user
+         redirect_to root_path
+      else
+         if (!user)
+            @error = "You don't belong here, do you?"
+         elsif (!user.authenticate(params[:session][:password]))
+            @error = "Tsk tsk."
+         end
+         render 'new'
+      end
+
+   end
+   def destroy
+      sign_out
+      redirect_to root_path
+   end
+end
